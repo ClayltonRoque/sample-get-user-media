a95eb70cd7f326f36d0b1a19954cffb035807c94
claylton_roque <claylton202@gmail.com>
2025-03-17 14:24:28 -0300

    configurado github e vercel deploy

--- src/components/VideoPreview.vue
+++ src/components/VideoPreview.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { watchEffect, shallowRef, useTemplateRef } from 'vue'
+import { watchEffect, shallowRef, useTemplateRef, computed } from 'vue'
 import Konva from 'konva'
 import * as faceapi from 'face-api.js'
 
@@ -7,25 +7,75 @@ const props = defineProps<{
     video: HTMLVideoElement
 }>()
 
+const models =
+    process.env.NODE_ENV === 'production' &&
+    process.env.DEPLOY_TARGET === 'github'
+        ? '/sample-get-user-media/weights'
+        : '/weights'
+
 const stage = shallowRef<Konva.Stage>()
+const videoImageBackground = shallowRef<Konva.Image>()
+const videoImageCenter = shallowRef<Konva.Image>()
 
 const container = useTemplateRef<HTMLDivElement>('container')
+const preview = useTemplateRef<HTMLImageElement>('preview')
+
+const screenWidth = computed(() => {
+    if (props.video.videoWidth > window.innerWidth) {
+        return window.innerWidth
+    }
+    return props.video.videoWidth
+})
+
+const screenHeight = computed(() => {
+    if (props.video.videoWidth > window.innerWidth) {
+        return (
+            (props.video.videoHeight / props.video.videoWidth) *
+            screenWidth.value
+        )
+    }
+    return props.video.videoHeight
+})
 
 const loadModels = async () => {
-    await faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-    await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(models)
+    await faceapi.nets.faceLandmark68Net.loadFromUri(models)
 }
 
-const detectFace = async () => {
-    await loadModels() // Aguarda o carregamento do modelo antes da detecção
+const detectFace = () => {
+    if (videoImageCenter.value && preview.value) {
+        const x = screenWidth.value / 4
+        const y = screenHeight.value / 10
+        const width = screenWidth.value / 2
+        const height = screenHeight.value / 1.25
 
-    const detections = await faceapi
-        .detectSingleFace(props.video)
-        .withFaceLandmarks()
+        videoImageCenter.value.toImage({
+            x,
+            y,
+            width,
+            height,
+            callback: (image) => {
+                // DEBUG
+                if (preview.value) {
+                    preview.value.src = image.src
+                }
 
-    console.log(detections)
+                faceapi
+                    .detectSingleFace(image)
+                    .withFaceLandmarks()
+                    .run()
+                    .then((detections) => {
+                        setTimeout(() => {
+                            console.log(detections)
+                            URL.revokeObjectURL(image.src)
+                            requestAnimationFrame(detectFace)
+                        }, 1000)
+                    })
+            },
+        })
+    }
 }
-detectFace() // Agora só executa após os modelos carregarem
+
 const updateVideo = () => {
     if (stage.value) {
         stage.value.batchDraw()
@@ -38,13 +88,10 @@ watchEffect(() => {
         return false
     }
 
-    const screenWidth = 1024
-    const screenHeight = 768
-
     stage.value = new Konva.Stage({
         container: container.value,
-        width: screenWidth,
-        height: screenHeight,
+        width: screenWidth.value,
+        height: screenHeight.value,
     })
 
     const layer = new Konva.Layer()
@@ -52,10 +99,10 @@ watchEffect(() => {
     const clipGroup = new Konva.Group({
         clipFunc: (ctx) => {
             ctx.ellipse(
-                screenWidth / 2,
-                screenHeight / 2.25,
-                screenWidth / 4,
-                screenHeight / 2.5,
+                screenWidth.value / 2,
+                screenHeight.value / 2.25,
+                screenWidth.value / 4,
+                screenHeight.value / 2.5,
                 0,
                 0,
                 Math.PI * 2
@@ -63,37 +110,46 @@ watchEffect(() => {
         },
     })
 
-    const videoImageBackground = new Konva.Image({
-        width: screenWidth,
-        height: screenHeight,
+    videoImageBackground.value = new Konva.Image({
+        width: screenWidth.value,
+        height: screenHeight.value,
         image: props.video,
         x: 0,
         y: 0,
         opacity: 0.4,
     })
 
-    const videoImageCenter = new Konva.Image({
-        width: screenWidth,
-        height: screenHeight,
+    videoImageCenter.value = new Konva.Image({
+        width: screenWidth.value,
+        height: screenHeight.value,
         image: props.video,
         x: 0,
         y: 0,
     })
 
-    layer.add(videoImageBackground)
+    layer.add(videoImageBackground.value)
 
-    clipGroup.add(videoImageCenter)
+    clipGroup.add(videoImageCenter.value)
 
     layer.add(clipGroup)
 
     stage.value.add(layer)
 
     updateVideo()
+    detectFace()
+
+    console.log(detectFace)
 })
+
+await loadModels() // Aguarda o carregamento do modelo antes da detecção
 </script>
 
 <template>
-    <div ref="container"></div>
+    <div>
+        <div ref="container"></div>
+        <!-- Use for debugging-->
+        <img ref="preview" />
+    </div>
 </template>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base:
+        process.env.NODE_ENV === 'production' &&
+        process.env.DEPLOY_TARGET === 'github'
+            ? '/sample-get-user-media'
+            : '/',
     plugins: [vue(), vueDevTools(), tailwindcss()],
+    build: {
+        outDir: process.env.DEPLOY_TARGET === 'github' ? 'docs' : 'dist',
+        assetsDir: 'assets',
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
